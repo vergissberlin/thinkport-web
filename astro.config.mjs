077034445e165d7c8e36ba2bff4534d0649a806c
André Lademann <alademann@thinkport.digital>
2025-01-17 20:40:00 +0100

    feat(astro.config.mjs): add webmanifest integration for PWA support
    Add astro-webmanifest to enhance PWA capabilities with a manifest
    file. This includes metadata like app name, icons, and theme colors.
    
    chore(package.json): add astro-webmanifest dependency
    
    Add astro-webmanifest to dependencies to support PWA features.
    
    style(favicon.svg): update favicon design for improved branding
    
    Update SVG path and styles to reflect new branding colors and design.
    
    chore(workspace.json): remove obsolete workspace state entries
    
    Clean up workspace.json by removing outdated entries and updating
    active and lastOpenFiles for better project organization.
    
    docs(first-post.md): add links to Azure and AWS training resources
    
    Enhance the first blog post with links to Azure and AWS training
    resources for better user guidance and resource accessibility.
    
    feat(images): add thinkport.svg logo to project for branding purposes
    
    docs(welcome.md): remove outdated Azure and AWS training links to streamline content

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,25 @@
 import {defineConfig} from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
+import webmanifest from 'astro-webmanifest'
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://thinkport.kubernaut.de',
-    integrations: [mdx(), sitemap()],
+    integrations: [
+        mdx(),
+        sitemap(),
+        webmanifest({
+            name: 'Thinkport',
+            icon: 'src/images/thinkport.svg',
+            short_name: 'TP',
+            description: 'Thinkport, cloud and beyond',
+            start_url: '/',
+            theme_color: '#0B2649',
+            background_color: '#0B2649',
+            display: 'standalone',
+        }),
+    ],
     redirects: {
         '/winter': {
             status: 302,
