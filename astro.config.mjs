81801181a838c75bc7acf481a9ba1427325057f2
André Lademann <alademann@thinkport.digital>
2025-01-17 18:51:33 +0100

    ci(deploy.yml): set NODE_ENV to production for deployment to ensure correct environment configuration
    fix(astro.config.mjs): correct base path for CI environment to '/thinkport-website' to match deployment structure
    
    feat(astro.config.mjs): add trailingSlash option set to "never" to standardize URL format
    
    feat(astro.config.mjs): add redirect from '/winter' to '/summer' with status 302 to handle outdated paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,13 @@ import sitemap from '@astrojs/sitemap'
 // https://astro.build/config
 export default defineConfig({
     site: 'https://vergissberlin.github.io',
-    base: process.env.CI ? '/thinkport-web/' : '/',
+    base: process.env.CI ? '/thinkport-website' : '/',
+    trailingSlash: "never",
     integrations: [mdx(), sitemap()],
+    redirects: {
+        '/winter': {
+            status: 302,
+            destination: '/summer',
+        },
+    }
 })
