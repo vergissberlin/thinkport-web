e9b70bcb7db289483e12fda35f04308dbc9bd34b
André Lademann <alademann@thinkport.digital>
2025-01-17 19:05:35 +0100

    build(deploy.yml): update Node.js version to 22 for compatibility with latest features
    refactor(astro.config.mjs): remove conditional base path logic for simplification
    
    fix(astro.config.mjs): update redirect destination from '/summer' to '/welcome' to reflect new content structure
    
    docs(winter.md): add newline for formatting consistency and update content to include 'winter' for clarity

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,13 @@ import sitemap from '@astrojs/sitemap'
 // https://astro.build/config
 export default defineConfig({
     site: 'https://vergissberlin.github.io',
-    base: process.env.NODE_ENV === 'production' ? 'thinkport-website' : '/',
+    base: 'thinkport-website',
     trailingSlash: "never",
     integrations: [mdx(), sitemap()],
     redirects: {
         '/winter': {
             status: 302,
-            destination: '/summer',
+            destination: '/welcome',
         },
     }
 })
--- src/pages/winter.md
+++ src/pages/winter.md
@@ -5,6 +5,7 @@ description: Lorem ipsum dolor sit amet
 pubDate: Jul 08 2024
 heroImage: /blog-placeholder-2.jpg
 ---
+
 ## Yellow white-paper
 
-Lorem ipsum
+Lorem ipsum winter
